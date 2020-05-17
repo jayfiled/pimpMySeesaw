ac4a7e4336b779257ddbddbb8df0c67f6258f603
Joel James <jayfiled@gmail.com>
2020-05-17 12:40:21 +1000

    Add buttons to activities that make them fly away when clicked

--- index.js
+++ index.js
@@ -28,5 +28,40 @@
     setTimeout(checkIfVidExists, 1000);
   };
 
+///----- Dismiss activities section -----/// 
+
+// Check local storage to see if any of the titles from the activities are in there, if they are, fly them away.
+
+  const addDismissButton = () => {
+    const el = document.getElementsByClassName("ss-prompt-view__container");
+    for (let i = 0; i < el.length - 1; i++) {
+      const button = document.createElement("div");
+
+      button.innerHTML = "✔";
+      button.style.backgroundColor = "rgba(130, 255, 96, .8)";
+      button.style.color = "#252A2A";
+      button.style.position = "absolute";
+      button.style.top = "16px";
+      button.style.right = "20px";
+      button.style.height = "39px";
+      button.style.width = "80px";
+      button.style.borderRadius = "500px";
+      button.style.boxShadow = "2px 2px 3px grey";
+      button.style.fontSize = "24px";
+      button.style.fontWeight = "800";
+      button.style.textAlign = "center";
+      button.addEventListener("click", (e) => flyAway(e));
+      el[i].appendChild(button);
+    }
+  };
+
+  // ? after dismissing 5 or 6, the layout looks weird - try making them fly further away
+  const flyAway = (e) => {
+// Get the title of the e.currentTarget and save to local storage as a key value
+    e.currentTarget.parentElement.style.position = "absolute";
+    e.currentTarget.parentElement.style.left = "-1000px";
+  };
+
+  addDismissButton();
   checkIfVidExists();
 })();
